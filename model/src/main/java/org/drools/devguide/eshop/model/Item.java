31e30fde2caffd87765726e96add8a0d52da3cd8
marianbuenosayres <marianbuenosayres@gmail.com>
2015-05-06 01:33:56 -0300

    Added first tests and cases for chapter 4

--- model/src/main/java/org/drools/devguide/eshop/model/Item.java
+++ model/src/main/java/org/drools/devguide/eshop/model/Item.java
@@ -6,7 +6,8 @@ import java.util.Objects;
 public class Item implements Serializable {
 
     public enum Category {
-        NA, LOW_RANGE, MID_RANGE, HIGH_RANGE
+        NA, LOW_RANGE, MID_RANGE, HIGH_RANGE,
+        SPECIAL_MIDHIGH_RANGE //used in chapter 4
     };
     private static final long serialVersionUID = 1L;
 
