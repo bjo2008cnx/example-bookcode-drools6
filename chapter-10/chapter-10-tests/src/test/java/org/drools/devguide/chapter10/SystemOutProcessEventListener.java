cd05e3e703824a6b0750864558d2a0ec36362c74
Salaboy <salaboy@gmail.com>
2016-01-18 15:47:52 +0000

    - updating chapter 10 test package name

--- chapter-10/chapter-10-tests/src/test/java/org/drools/devguide/chapter10/SystemOutProcessEventListener.java
+++ chapter-10/chapter-10-tests/src/test/java/org/drools/devguide/chapter10/SystemOutProcessEventListener.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.drools.devguide.chapter08;
+package org.drools.devguide.chapter10;
 
 import org.kie.api.event.process.ProcessCompletedEvent;
 import org.kie.api.event.process.ProcessNodeLeftEvent;
